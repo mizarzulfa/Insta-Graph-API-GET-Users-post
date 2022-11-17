bd46ef1fa764ab9f39f3e305e44c1ec07ba3f38f
mizar <mizarzulfaz@gmail.com>
2022-11-17 19:40:56 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+INSTAGRAM GRAPH API - GET USER MEDIA
+ check out the live version here : https://mizarzulfa.com --> Featured Contents Section
---
+++ credentials.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+
+	define( 'FACEBOOK_APP_ID', '<FACEBOOK_APP_ID>' );
+	define( 'FACEBOOK_APP_SECRET', '<FACEBOOK_APP_SECRET>' );
+	define( 'FACEBOOK_REDIRECT_URI', '<FACEBOOK_REDIRECT_URI>' );
+	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v15.0/' ); //Last Update Nov 12,2022
+
+	// accessToken (LONG LIVED TOKEN)
+	$accessToken = '<accessToken>';
+
+	// ur page id
+	$pageId = '<pageId>';
+
+	// ur instagram business account id
+	 $instagramAccountId = '<instagramAccountId>';
+
+    ?>	
